a2d4d9f8e713e4c5eddd4376eae03a968116b6dd
zLagaduPro <cauedejesussantos12345@gmail.com>
2021-02-26 04:02:33 -0300

    NLW04 - Dia 05

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,9 +7,7 @@ import { ChallengesProvider } from '../contexts/ChallengesContext';
 function MyApp({ Component, pageProps }) {
 
   return (
-    <ChallengesProvider>
         <Component {...pageProps} />
-    </ChallengesProvider>
   );
 }
 
